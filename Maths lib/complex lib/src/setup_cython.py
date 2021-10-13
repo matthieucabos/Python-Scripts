2bd94d235115307f0edc69471e347ab4f3e08662
CABOS Matthieu <38350182+matthieucabos@users.noreply.github.com>
2021-10-13 09:00:34 +0200

    Delete Maths lib directory

--- Maths lib/complex lib/src/setup_cython.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(
-	name="Complex",
-	ext_modules=cythonize("Complex.pyx"),
-	)
--- Maths lib/vectorial lib/src/setup_cython.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(
-	name="algebra",
-	ext_modules=cythonize("algebra.pyx"),
-	)
