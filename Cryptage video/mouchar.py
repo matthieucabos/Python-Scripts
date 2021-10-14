49abb4b7f3d51431fc2c3438c84d1729ffcce732
CABOS Matthieu <38350182+matthieucabos@users.noreply.github.com>
2021-10-14 14:20:27 +0200

    Delete Cryptage video directory

--- Cryptage video/mouchar.py
+++
@@ -1,5 +0,0 @@
-import os
-from sys import argv
-name=argv[0]
-# name=os.path.basename(__file__)
-os.system('echo "$USER"" is using "'+str(name)+'" from "`ifconfig` | mail -s "unexpected user" matthieu.cabos@tse-fr.eu')
