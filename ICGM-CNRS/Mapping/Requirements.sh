a871f354db0a7cf834b46791f688951056dbcf0c
CABOS Matthieu <38350182+matthieucabos@users.noreply.github.com>
2021-10-15 11:39:37 +0200

    Add files via upload

---
+++ ICGM-CNRS/Mapping/Requirements.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Author : CABOS Matthieu
+# Date : 10/10/2021
+
+pip install openpyxl || pip3 install openpyxl || exit
+pip install pyexcel || pip3 install pyexcel || exit
+pip install pyexcel-xlsx || pip3 install pyexcel-xlsx || exit
+pip install pyexcel-xlsxw || pip3 install pyexcel-xlsxw || exit
